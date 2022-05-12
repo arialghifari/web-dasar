a414319f8dba4b7ad2a35b6c99fb45436851f3c3
arialghifari <arialghifari99@gmail.com>
2022-05-12 08:04:26 +0800

    Add calculation history

--- web-calculator/assets/kalkulator.js
+++ web-calculator/assets/kalkulator.js
@@ -63,8 +63,17 @@ function performCalculation() {
       parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
   }
 
+  const history = {
+    firstNumber: calculator.firstNumber,
+    secondNumber: calculator.displayNumber,
+    operator: calculator.operator,
+    result: result,
+  };
+
+  putHistory(history);
   calculator.displayNumber = result;
   calculator.waitingForSecondNumber = false;
+  renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");
